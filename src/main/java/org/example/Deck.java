4e11fd8630b5847943f4534d12acb5cb1f926c1f
ArdaDol <ardadolanay@gmail.com>
2023-10-19 21:21:09 -0400

    Code:002:  add functionality for deck

--- src/main/java/org/example/Deck.java
+++ src/main/java/org/example/Deck.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 public class Deck {
 
     private ArrayList<Card> deck;
-
+    private final String[] colors = {"Swrd","Arrw","Decp","Sorc", "Merl", "Appr", "Alch"};
     public Deck() {
 
         /*
@@ -19,8 +19,7 @@ public class Deck {
 
 
         deck = new ArrayList<Card>();
-        String[] colors = {"Swrd","Arrw","Decp","Sorc"};
-        String[] specialnumbers = {"Merl", "Appr", "Alch"};
+
 
         deck.add(new Card(1, colors[0],5));
         deck.add(new Card(2, colors[0],5));
@@ -54,49 +53,49 @@ public class Deck {
         deck.add(new Card(14, colors[1],5));
         deck.add(new Card(15, colors[1],5));
 
-        deck.add(new Card(1, colors[1],5));
-        deck.add(new Card(2, colors[1],5));
-        deck.add(new Card(3, colors[1],5));
-        deck.add(new Card(4, colors[1],5));
-        deck.add(new Card(5, colors[1],10));
-        deck.add(new Card(6, colors[1],10));
-        deck.add(new Card(7, colors[1],5));
-        deck.add(new Card(8, colors[1],5));
-        deck.add(new Card(9, colors[1],5));
-        deck.add(new Card(10, colors[1],5));
-        deck.add(new Card(11, colors[1],10));
-        deck.add(new Card(12, colors[1],10));
-        deck.add(new Card(13, colors[1],5));
-        deck.add(new Card(14, colors[1],5));
-        deck.add(new Card(15, colors[1],5));
+        deck.add(new Card(1, colors[2],5));
+        deck.add(new Card(2, colors[2],5));
+        deck.add(new Card(3, colors[2],5));
+        deck.add(new Card(4, colors[2],5));
+        deck.add(new Card(5, colors[2],10));
+        deck.add(new Card(6, colors[2],10));
+        deck.add(new Card(7, colors[2],5));
+        deck.add(new Card(8, colors[2],5));
+        deck.add(new Card(9, colors[2],5));
+        deck.add(new Card(10, colors[2],5));
+        deck.add(new Card(11, colors[2],10));
+        deck.add(new Card(12, colors[2],10));
+        deck.add(new Card(13, colors[2],5));
+        deck.add(new Card(14, colors[2],5));
+        deck.add(new Card(15, colors[2],5));
 
-        deck.add(new Card(1, colors[1],5));
-        deck.add(new Card(2, colors[1],5));
-        deck.add(new Card(3, colors[1],5));
-        deck.add(new Card(4, colors[1],5));
-        deck.add(new Card(5, colors[1],5));
-        deck.add(new Card(6, colors[1],10));
-        deck.add(new Card(7, colors[1],10));
-        deck.add(new Card(8, colors[1],5));
-        deck.add(new Card(9, colors[1],10));
-        deck.add(new Card(10, colors[1],10));
-        deck.add(new Card(11, colors[1],5));
-        deck.add(new Card(12, colors[1],5));
-        deck.add(new Card(13, colors[1],5));
-        deck.add(new Card(14, colors[1],5));
-        deck.add(new Card(15, colors[1],5));
+        deck.add(new Card(1, colors[3],5));
+        deck.add(new Card(2, colors[3],5));
+        deck.add(new Card(3, colors[3],5));
+        deck.add(new Card(4, colors[3],5));
+        deck.add(new Card(5, colors[3],5));
+        deck.add(new Card(6, colors[3],10));
+        deck.add(new Card(7, colors[3],10));
+        deck.add(new Card(8, colors[3],5));
+        deck.add(new Card(9, colors[3],10));
+        deck.add(new Card(10, colors[3],10));
+        deck.add(new Card(11, colors[3],5));
+        deck.add(new Card(12, colors[3],5));
+        deck.add(new Card(13, colors[3],5));
+        deck.add(new Card(14, colors[3],5));
+        deck.add(new Card(15, colors[3],5));
 
 
         for(int i =0; i<3;i++) {
-            deck.add(new Card(0, specialnumbers[0], 25));
+            deck.add(new Card(0, colors[4], 25));
         }
         for(int i=0;i<2;i++){
-            deck.add(new Card(0,specialnumbers[1], 5));
+            deck.add(new Card(0,colors[5], 5));
 
         }
 
         for(int i=0;i<15;i++){
-            deck.add(new Card(0,specialnumbers[1],5));
+            deck.add(new Card(0,colors[6],5));
         }
 
 
@@ -113,9 +112,9 @@ public class Deck {
     }
 
     public boolean addCard(int value, String colour, int  damage){
-        String[] colours = {"Swrd","Arrw","Decp","Sorc", "Merl", "Appr", "Alch"};
-        for(int i=0; i<colours.length;i++) {
-            if (colour == colours[i]){
+
+        for(int i=0; i<colors.length;i++) {
+            if (colour == colors[i]){
                 if(value<=15 && value >=0){
                     deck.add(new Card(value, colour, damage));
                     return true;
@@ -131,7 +130,7 @@ public class Deck {
          * Checks the size of the deck. If it is greater than 0 then returns false. If not, returns true
          */
 
-        if(deck.size()>0) {
+        if(!deck.isEmpty()) {
             return false;
         }
         return true;
--- src/test/java/org/example/MainUnitTest.java
+++ src/test/java/org/example/MainUnitTest.java
@@ -8,11 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MainUnitTest {
 
     @Test
-   //DisplayName("U-Test 002:Test to see if the right card is added to the deck")
+   //DisplayName("U-Test 002:Test to see if the right card is remvoed")
     void TestToString(){
         Deck deck = new Deck();
 
-        //deck.addCard(1,"Test",0);
+        deck.addCard(1,"Test1",0);
+        deck.addCard(1,"Test2",0);
+        deck.addCard(1,"Test3",0);
 
 
 
